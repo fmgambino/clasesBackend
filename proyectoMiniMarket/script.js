c99248589f5f5f4a6dc7f3b87546d39d638458d6
fmgambino <fernando.m.gambino@gmail.com>
2024-06-12 22:07:18 -0300

    update

--- proyectoMiniMarket/script.js
+++ proyectoMiniMarket/script.js
@@ -2,9 +2,9 @@ const modeToggle = document.getElementById('modeToggle');
 modeToggle.addEventListener('change', () => {
     if (modeToggle.checked) {
         document.body.classList.add('dark-mode');
-        modeToggle.nextElementSibling.textContent = 'Modo Claro';
+        modeToggle.nextElementSibling.textContent = '';
     } else {
         document.body.classList.remove('dark-mode');
-        modeToggle.nextElementSibling.textContent = 'Modo Oscuro';
+        modeToggle.nextElementSibling.textContent = '';
     }
 });
